5e9cf8bbd868d0f29c6974e2b868cd11a1789c24
AndriiRi <riazanovandrii@gmail.com>
2022-11-14 16:37:35 +0200

    config change

--- next.config.js
+++ next.config.js
@@ -5,8 +5,4 @@ const nextConfig = {
   unoptimized: true,
 };
 
-module.exports = {
-  images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-  },
-};
+module.exports = nextConfig;
